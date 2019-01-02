7bc4a41c349dbf080613a8c2471aaf06f02a1e14
José Aniceto <jose.pedro.aniceto@gmail.com>
2019-01-02 20:23:07 +0000

    Created PROFILE page for each user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.ImageField(default='images/profile_photos/default.jpg', upload_to='images/profile_photos')
+    bio = models.TextField(blank=True)
+    scholar_url = models.URLField(blank=True)
+    linkedin_url = models.URLField(blank=True)
+    scopus_url = models.URLField(blank=True)
+    cv_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
