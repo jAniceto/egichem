3896def3ceee06f8e9b1f384a9c7178e2e85ffa6
José Aniceto <jose.pedro.aniceto@gmail.com>
2019-01-03 21:49:32 +0000

    Modified cropping and resizing of images for profile

--- users/models.py
+++ users/models.py
@@ -13,23 +13,27 @@ class Profile(models.Model):
     scopus_url = models.URLField(blank=True)
     personal_url = models.URLField(blank=True)
 
+    
     def __str__(self):
         return f'{self.user.username} Profile'
     
+    
+    # Override the save method to handle image upload (reduce size and crop)
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
         img = Image.open(self.photo.path)
         
-        # Crop image to square
-        w, h = img.size  
+        # Reduce size of image
         if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            
+            # Crop image to square
             if img.width != img.height:
+                w, h = img.size
                 cropped = img.crop((w//2 - 300//2, h//2 - 300//2, w//2 + 300//2, h//2 + 300//2))
                 cropped.save(self.photo.path)
 
-            # Reduce size of image
             else:
-                output_size = (300, 300)
-                img.thumbnail(output_size)
                 img.save(self.photo.path)
