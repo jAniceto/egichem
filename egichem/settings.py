5599592c81fc439ab8b01806099bf5ad4c0cd6f6
José Aniceto <jose.pedro.aniceto@gmail.com>
2019-01-12 00:47:21 +0000

    Added Password reset functionality

--- egichem/settings.py
+++ egichem/settings.py
@@ -11,10 +11,14 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Load configuration file
+with open(os.path.join(BASE_DIR, 'config.json')) as f:
+    config_file = json.load(f)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -137,3 +141,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config_file['EMAIL_USER']
+EMAIL_HOST_PASSWORD = config_file['EMAIL_PASS']
