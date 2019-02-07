c7d367d934025cbf39dcc27fb4d79d54fb732c11
José Aniceto <jose.pedro.aniceto@gmail.com>
2019-02-07 22:47:36 +0000

    Debugging

--- egichem/settings.py
+++ egichem/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y_hncujhp4ksg!)8!atotnm#sr8qj9xgs+9d0rg6molq%chtv*'
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
