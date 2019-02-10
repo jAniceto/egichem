345979f58222db9422e8aff0c83ce0a23b2c6435
José Aniceto <jose.pedro.aniceto@gmail.com>
2019-02-10 20:55:20 +0000

    Added search function to inventory

--- lab/views.py
+++ lab/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.db.models import Q
 
 from .models import Material
 
@@ -12,7 +13,7 @@ class MaterialListView(PermissionRequiredMixin, LoginRequiredMixin, ListView):
     template_name = 'lab/inventory.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'materials'
     ordering = ['name']
-    paginate_by = 10
+    paginate_by = 25
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
@@ -20,6 +21,17 @@ class MaterialListView(PermissionRequiredMixin, LoginRequiredMixin, ListView):
         # Add in extra context variables
         context['page_title'] = 'inventory'
         return context
+    
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            object_list = self.model.objects.filter(
+                Q(name__icontains=query) | 
+                Q(specifications__icontains=query)
+            ).distinct().order_by('name')
+        else:
+            object_list = self.model.objects.all().order_by('name')
+        return object_list
 
 
 class MaterialCreateView(LoginRequiredMixin, CreateView):
