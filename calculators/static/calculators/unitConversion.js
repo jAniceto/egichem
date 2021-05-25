eef1bdaef2ae76b81014d6fb437ffe935001f761
jAniceto <jose.pedro.aniceto@gmail.com>
2021-05-25 18:10:04 +0100

    Added new units and new way to make conversions

--- calculators/static/calculators/unitConversion.js
+++ calculators/static/calculators/unitConversion.js
@@ -107,31 +107,192 @@ function quickConvert() {
 // -------------------
 var unitsByKind = [
   {
-    "kind": "time",
+    "kind": "area",
     "units": [
       {
-        "id": "second",
-        "str": "second (s)"
+        "id": "cm2",
+        "str": "square centimeter (cm2)",
+        "factor": 1e-4
       },
       {
-        "id": "minute",
-        "str": "minute (min)"
+        "id": "m2",
+        "str": "square meter (m2)",
+        "factor": 1
       },
       {
-        "id": "hour",
-        "str": "hour (h)"
+        "id": "km2",
+        "str": "square kilometer (km2)",
+        "factor": 1000000
       },
       {
-        "id": "day",
-        "str": "day"
+        "id": "acre",
+        "str": "acre",
+        "factor": 4046.85642
       },
       {
-        "id": "week",
-        "str": "week"
+        "id": "hectare",
+        "str": "hectare (ha)",
+        "factor": 10000
+      },
+    ]
+  },
+  {
+    "kind": "concentration",
+    "units": [
+      {
+        "id": "mmol/ml",
+        "str": "milimol per mililiter (mmol/ml)",
+        "factor": 1000
       },
       {
-        "id": "year",
-        "str": "year"
+        "id": "mol/l",
+        "str": "mol per liter (mol/l)",
+        "factor": 1000
+      },
+      {
+        "id": "mol/cm3",
+        "str": "mol per cubic centimeter (mol/cm3)",
+        "factor": 1e6
+      },
+      {
+        "id": "mol/dm3",
+        "str": "mol per cubic decimeter (mol/dm3)",
+        "factor": 1000
+      },
+      {
+        "id": "mol/m3",
+        "str": "mol per cubic meter (mol/m3)",
+        "factor": 1
+      }
+    ]
+  },
+  {
+    "kind": "density",
+    "units": [
+      {
+        "id": "g/ml",
+        "str": "gram per mililiter (g/ml)",
+        "factor": 1000
+      },
+      {
+        "id": "g/l",
+        "str": "gram per liter (g/l)",
+        "factor": 1
+      },
+      {
+        "id": "kg/l",
+        "str": "kilogram per liter (kg/l)",
+        "factor": 1000
+      },
+      {
+        "id": "g/cm3",
+        "str": "gram per cubic centimeter (g/cm3)",
+        "factor": 1000
+      },
+      {
+        "id": "kg/dm3",
+        "str": "kilogram per cubic decimeter (kg/dm3)",
+        "factor": 1000
+      },
+      {
+        "id": "kg/m3",
+        "str": "kilogram per cubic meter (kg/m3)",
+        "factor": 1
+      },
+    ]
+  },
+  {
+    "kind": "diffusivity",
+    "units": [
+      {
+        "id": "cm2/s",
+        "str": "centimeter square per second (cm2/s)",
+        "factor": 1e-4
+      },
+      {
+        "id": "m2/s",
+        "str": "meter square per second (m2/s)",
+        "factor": 1
+      },
+      {
+        "id": "cm2/min",
+        "str": "centimeter square per minute (cm2/min)",
+        "factor": 1/600000
+      },
+      {
+        "id": "m2/min",
+        "str": "meter square per minute (m2/min)",
+        "factor": 1/60
+      },
+    ]
+  },
+  {
+    "kind": "energy",
+    "units": [
+      {
+        "id": "joule",
+        "str": "joule (J)",
+        "factor": 1
+      },
+      {
+        "id": "kJ",
+        "str": "kilojoule (kJ)",
+        "factor": 1000
+      },
+      {
+        "id": "MJ",
+        "str": "megajoule (MJ)",
+        "factor": 1e6
+      },
+      {
+        "id": "GJ",
+        "str": "gigajoule (GJ)",
+        "factor": 1e9
+      },
+      {
+        "id": "calorie",
+        "str": "calorie (cal)",
+        "factor": 4.184
+      },
+      {
+        "id": "kilocalorie",
+        "str": "kilocalorie (kcal)",
+        "factor": 4184
+      },
+      {
+        "id": "btu",
+        "str": "british thermal unit (BTU)",
+        "factor": 1055.05585
+      },
+      {
+        "id": "erg",
+        "str": "erg",
+        "factor": 1e-7
+      },
+    ]
+  },
+  {
+    "kind": "specific heat capacity",
+    "units": [
+      {
+        "id": "J/(kg K)",
+        "str": "joule per kilogram-Kelvin (J/(kg K))",
+        "factor": 1
+      },
+      {
+        "id": "J/(g °C)",
+        "str": "joule per gram-Celsius (J/(g °C))",
+        "factor": 1000
+      },
+      {
+        "id": "cal/(g °C)",
+        "str": "calorie per gram-Celsius (cal/(g °C))",
+        "factor": 4186.8
+      },
+      {
+        "id": "BTU/(lb °F)",
+        "str": "BTU per pound-Fahrenheit (BTU/(lb °F))",
+        "factor": 4186.8
       },
     ]
   },
@@ -140,101 +301,63 @@ var unitsByKind = [
     "units": [
       {
         "id": "angstrom",
-        "str": "Ångström (Å)"
+        "str": "Ångström (Å)",
+        "factor": 1e-10
       },
       {
         "id": "foot",
-        "str": "foot (ft)"
+        "str": "foot (ft)",
+        "factor": 0.3048
       },
       {
         "id": "inch",
-        "str": "inch (in)"
+        "str": "inch (in)",
+        "factor": 0.0254
       },
       {
         "id": "yard",
-        "str": "yard (yd)"
+        "str": "yard (yd)",
+        "factor": 0.9144
       },
       {
         "id": "mile",
-        "str": "mile (mi)"
+        "str": "mile (mi)",
+        "factor": 1609.344
       },
       {
         "id": "nm",
-        "str": "nanometer (nm)"
+        "str": "nanometer (nm)",
+        "factor": 1e-9
       },
       {
         "id": "um",
-        "str": "micrometer (um)"
+        "str": "micrometer (um)",
+        "factor": 1e-6
+      },
+      {
+        "id": "mm",
+        "str": "milimeter (mm)",
+        "factor": 0.001
       },
       {
         "id": "cm",
-        "str": "centimeter (cm)"
+        "str": "centimeter (cm)",
+        "factor": 0.01
       },
       {
         "id": "dm",
-        "str": "decimeter (dm)"
+        "str": "decimeter (dm)",
+        "factor": 0.1
       },
       {
         "id": "m",
-        "str": "meter (m)"
+        "str": "meter (m)",
+        "factor": 1
       },
       {
         "id": "km",
-        "str": "kilometer (km)"
-      },
-    ]
-  },
-  {
-    "kind": "area",
-    "units": [
-      {
-        "id": "cm2",
-        "str": "square centimeter (cm2)"
-      },
-      {
-        "id": "m2",
-        "str": "square meter (m2)"
-      },
-      {
-        "id": "km2",
-        "str": "square kilometer (km2)"
-      },
-      {
-        "id": "acre",
-        "str": "acre"
-      },
-      {
-        "id": "hectare",
-        "str": "hectare (ha)"
-      },
-    ]
-  },
-  {
-    "kind": "volume",
-    "units": [
-      {
-        "id": "gallon",
-        "str": "gallon (gal)"
-      },
-      {
-        "id": "ml",
-        "str": "mililiter (ml)"
-      },
-      {
-        "id": "l",
-        "str": "liter (l)"
-      },
-      {
-        "id": "cm3",
-        "str": "cubic centimeter (cm3)"
-      },
-      {
-        "id": "dm3",
-        "str": "cubic decimeter (dm3)"
-      },
-      {
-        "id": "m3",
-        "str": "cubic meter (m3)"
+        "str": "kilometer (km)",
+        "factor": 1000
       },
     ]
   },
@@ -243,27 +366,143 @@ var unitsByKind = [
     "units": [
       {
         "id": "mg",
-        "str": "miligram (mg)"
+        "str": "miligram (mg)",
+        "factor": 1e-6
       },
       {
         "id": "gram",
-        "str": "gram (g)"
+        "str": "gram (g)",
+        "factor": 0.001
       },
       {
         "id": "kg",
-        "str": "kilogram"
+        "str": "kilogram (kg)",
+        "factor": 1
       },
       {
         "id": "metric-ton",
-        "str": "tonnne (t)"
+        "str": "tonnne (t)",
+        "factor": 1000
       },
       {
         "id": "ounce",
-        "str": "ounce (oz)"
+        "str": "ounce (oz)",
+        "factor": 0.0283495231
       },
       {
         "id": "pound",
-        "str": "pound (lb)"
+        "str": "pound (lb)",
+        "factor": 0.45359237
+      },
+    ]
+  },
+  {
+    "kind": "permeability",
+    "units": [
+      {
+        "id": "mol m/(m2 s Pa)",
+        "str": "mol m/(m2 s Pa)",
+        "factor": 1
+      },
+      {
+        "id": "barrer",
+        "str": "barrer",
+        "factor": 3.35e-16
+      },
+      {
+        "id": "cm3STP cm/(cm2 s cmHg)",
+        "str": "cm3STP cm/(cm2 s cmHg)",
+        "factor": 3.35e-6
+      },
+    ]
+  }, 
+  {
+    "kind": "permeance",
+    "units": [
+      {
+        "id": "mol/(m2 s Pa)",
+        "str": "mol/(m2 s Pa)",
+        "factor": 1
+      },
+      {
+        "id": "cm3STP/(cm2 s cmHg)",
+        "str": "cm3STP/(cm2 s cmHg)",
+        "factor": 3.35e-4
+      },
+      {
+        "id": "m3STP/(m2 s Pa)",
+        "str": "m3STP/(m2 s Pa)",
+        "factor": 44.6607119051
+      },
+      {
+        "id": "GPU",
+        "str": "gas permeance unit (GPU)",
+        "factor": 3.35e-10
+      },
+    ]
+  }, 
+  {
+    "kind": "pressure",
+    "units": [
+      {
+        "id": "atm",
+        "str": "atmosphere (atm)",
+        "factor": 101325
+      },
+      {
+        "id": "bar",
+        "str": "bar",
+        "factor": 100000
+      },
+      {
+        "id": "mmHg",
+        "str": "millimetre of mercury (mmHg)",
+        "factor": 133.322368
+      },
+      {
+        "id": "pascal",
+        "str": "pascal (Pa)",
+        "factor": 1
+      },
+      {
+        "id": "kilopascal",
+        "str": "kilopascal (kPa)",
+        "factor": 1000
+      },
+      {
+        "id": "megapascal",
+        "str": "megapascal (MPa)",
+        "factor": 1e6
+      },
+      {
+        "id": "psi",
+        "str": "pounds per square inch (psi)",
+        "factor": 6894.75729
+      },
+      {
+        "id": "torr",
+        "str": "torr",
+        "factor": 133.322368
+      },
+    ]
+  }, 
+  {
+    "kind": "speed",
+    "units": [
+      {
+        "id": "m s-1",
+        "str": "meter per second (m/s)",
+        "factor": 1
+      },
+      {
+        "id": "kph",
+        "str": "kilometer per hour (km/h)",
+        "factor": 0.277777778
+      },
+      {
+        "id": "mph",
+        "str": "miles per hour (mph)",
+        "factor": 0.44704
       },
     ]
   },
@@ -272,193 +511,74 @@ var unitsByKind = [
     "units": [
       {
         "id": "tempC",
-        "str": "Celsius (°C)"
+        "str": "Celsius (°C)",
       },
       {
         "id": "tempK",
-        "str": "Kelvin (K)"
+        "str": "Kelvin (K)",
       },
       {
         "id": "tempF",
-        "str": "Fahrenheit (°F)"
+        "str": "Fahrenheit (°F)",
       },
       {
         "id": "tempR",
-        "str": "Rankine (°R)"
+        "str": "Rankine (°R)",
       },
     ]
   },
   {
-    "kind": "pressure",
+    "kind": "thermal conductivity",
     "units": [
       {
-        "id": "atm",
-        "str": "atmosphere (atm)"
+        "id": "W/(cm K)",
+        "str": "watts per centimeter-Kelvin (W/(cm K))",
+        "factor": 100
       },
       {
-        "id": "bar",
-        "str": "bar"
+        "id": "W/(m K)",
+        "str": "watts per meter-Kelvin (W/(m K))",
+        "factor": 1
       },
       {
-        "id": "mmHg",
-        "str": "millimetre of mercury (mmHg)"
-      },
-      {
-        "id": "pascal",
-        "str": "pascal (Pa)"
-      },
-      {
-        "id": "kilopascal",
-        "str": "kilopascal (kPa)"
-      },
-      {
-        "id": "megapascal",
-        "str": "megapascal (MPa)"
-      },
-      {
-        "id": "psi",
-        "str": "pounds per square inch (psi)"
-      },
-      {
-        "id": "torr",
-        "str": "torr"
-      },
-    ]
-  }, 
-  {
-    "kind": "energy",
-    "units": [
-      {
-        "id": "joule",
-        "str": "joule (J)"
-      },
-      {
-        "id": "kJ",
-        "str": "kilojoule (kJ)"
-      },
-      {
-        "id": "MJ",
-        "str": "megajoule (MJ)"
-      },
-      {
-        "id": "GJ",
-        "str": "gigajoule (GJ)"
-      },
-      {
-        "id": "calorie",
-        "str": "calorie (cal)"
-      },
-      {
-        "id": "kilocalorie",
-        "str": "kilocalorie (kcal)"
-      },
-      {
-        "id": "btu",
-        "str": "british thermal unit (BTU)"
-      },
-      {
-        "id": "erg",
-        "str": "erg"
+        "id": "BTU/(h ft F)",
+        "str": "BTU per hour-foot-Fahrenheit (BTU/(h ft °F))",
+        "factor": 1.7295772056
       },
     ]
   },
   {
-    "kind": "speed",
+    "kind": "time",
     "units": [
       {
-        "id": "m s-1",
-        "str": "meter per second (m/s)"
+        "id": "second",
+        "str": "second (s)",
+        "factor": 1
       },
       {
-        "id": "kph",
-        "str": "kilometer per hour (km/h)"
+        "id": "minute",
+        "str": "minute (min)",
+        "factor": 60
       },
       {
-        "id": "mph",
-        "str": "miles per hour (mph)"
-      },
-    ]
-  },
-  {
-    "kind": "volumetric_flow",
-    "units": [
-      {
-        "id": "m3/s",
-        "str": "cubic meter per second (m3/s)"
+        "id": "hour",
+        "str": "hour (h)",
+        "factor": 3600
       },
       {
-        "id": "m3/h",
-        "str": "cubic meter per hour (m3/h)"
+        "id": "day",
+        "str": "day",
+        "factor": 86400
       },
       {
-        "id": "m3/day",
-        "str": "cubic meter per day (m3/day)"
+        "id": "week",
+        "str": "week",
+        "factor": 604800
       },
       {
-        "id": "ml/s",
-        "str": "mililiter per second (ml/s)"
-      },
-      {
-        "id": "ml/min",
-        "str": "mililiter per minute (ml/min)"
-      },
-      {
-        "id": "l/h",
-        "str": "liter per hour (h)"
-      },
-    ]
-  },
-  // {
-  //   "kind": "concentration",
-  //   "units": [
-  //     {
-  //       "id": "mmol/ml",
-  //       "str": "milimol per mililiter (mmol/ml)"
-  //     },
-  //     {
-  //       "id": "mol/l",
-  //       "str": "mol per liter (mol/l)"
-  //     },
-  //     {
-  //       "id": "mol/cm3",
-  //       "str": "mol per cubic centimeter (mol/cm3)"
-  //     },
-  //     {
-  //       "id": "mol/dm3",
-  //       "str": "mol per cubic decimeter (mol/dm3)"
-  //     },
-  //     {
-  //       "id": "mol/m3",
-  //       "str": "mol per cubic meter (mol/m3)"
-  //     }
-  //   ]
-  // },
-  {
-    "kind": "density",
-    "units": [
-      {
-        "id": "g/ml",
-        "str": "gram per mililiter (g/ml)"
-      },
-      {
-        "id": "g/l",
-        "str": "gram per liter (g/l)"
-      },
-      {
-        "id": "kg/l",
-        "str": "kilogram per liter (kg/l)"
-      },
-      {
-        "id": "g/cm3",
-        "str": "gram per cubic centimeter (g/cm3)"
-      },
-      {
-        "id": "kg/dm3",
-        "str": "kilogram per cubic decimeter (kg/dm3)"
-      },
-      {
-        "id": "kg/m3",
-        "str": "kilogram per cubic meter (kg/m3)"
+        "id": "year",
+        "str": "year",
+        "factor": 31556926
       },
     ]
   },
@@ -467,23 +587,103 @@ var unitsByKind = [
     "units": [
       {
         "id": "mP",
-        "str": "milipoise (mP)"
+        "str": "milipoise (mP)",
+        "factor": 0.0001
       },
       {
         "id": "cP",
-        "str": "centipoise (mP)"
+        "str": "centipoise (mP)",
+        "factor": 0.001
       },
       {
         "id": "P",
-        "str": "poise (P)"
+        "str": "poise (P)",
+        "factor": 0.1
       },
       {
         "id": "mPa s",
-        "str": "milipascal second (Pa s)"
+        "str": "milipascal second (mPa s)",
+        "factor": 0.001
       },
       {
         "id": "Pa s",
-        "str": "pascal second (Pa s)"
+        "str": "pascal second (Pa s)",
+        "factor": 1
+      },
+    ]
+  },
+  {
+    "kind": "volume",
+    "units": [
+      {
+        "id": "gallon",
+        "str": "gallon (gal)",
+        "factor": 0.003785411784
+      },
+      {
+        "id": "ml",
+        "str": "mililiter (ml)",
+        "factor": 1e-6
+      },
+      {
+        "id": "l",
+        "str": "liter (l)",
+        "factor": 1e-3
+      },
+      {
+        "id": "cm3",
+        "str": "cubic centimeter (cm3)",
+        "factor": 1e-6
+      },
+      {
+        "id": "dm3",
+        "str": "cubic decimeter (dm3)",
+        "factor": 1e-3
+      },
+      {
+        "id": "m3",
+        "str": "cubic meter (m3)",
+        "factor": 1
+      },
+    ]
+  },
+  {
+    "kind": "volumetric_flow",
+    "units": [
+      {
+        "id": "m3/s",
+        "str": "cubic meter per second (m3/s)",
+        "factor": 1
+      },
+      {
+        "id": "m3/h",
+        "str": "cubic meter per hour (m3/h)",
+        "factor": 3600
+      },
+      {
+        "id": "m3/day",
+        "str": "cubic meter per day (m3/day)",
+        "factor": 1.15740741e-5
+      },
+      {
+        "id": "ml/s",
+        "str": "mililiter per second (ml/s)",
+        "factor": 1e-6
+      },
+      {
+        "id": "ml/min",
+        "str": "mililiter per minute (ml/min)",
+        "factor": 1.66666667e-8
+      },
+      {
+        "id": "l/h",
+        "str": "liter per hour (l/h)",
+        "factor": 2.7777777777778e-7
+      },
+      {
+        "id": "gal/s",
+        "str": "gallon per second (gal/s)",
+        "factor": 0.00378541178
       },
     ]
   },
@@ -521,10 +721,21 @@ fromUnitSelect.addEventListener('change', function() {
     createSelectOptions('fromUnitSelect');
     createSelectOptions('toUnitSelect');
   } else {
-    var kind = Qty.parse(fromUnitSelect.value).kind();
+    var kind = getKindById(fromUnitSelect.value);
     filterSelectOptions('toUnitSelect', kind);
   }
 });
+
+
+// Function to get kind of quantity from a id
+function getKindById(idValue) {
+  let quantitySelected = unitsByKind.find(quantity => {
+    return quantity.units.some(unit => {
+      return unit.id === idValue;
+    });
+  })
+  return quantitySelected.kind;
+}
 
 
 // Function to populate the options of the unit select widget
@@ -573,8 +784,40 @@ function detailedConvert() {
     detailedResult.value = +result.scalar.toPrecision(6);
 
   } catch (error) {
-    detailedConversionAlert.style.display = 'block';
-    detailedConversionAlert.innerHTML = error;
+    try {
+      
+      let resultManual = manualConversion(detailedFrom.value, fromUnitSelect.value, toUnitSelect.value)
+
+      detailedResult.value = resultManual.toPrecision(6);
+
+    } catch (error2) {
+      detailedConversionAlert.style.display = 'block';
+      detailedConversionAlert.innerHTML = error;
+    }
   }
 
+}
+
+
+// Function to get unit object
+function getUnitObjectById(idValue) {
+  let quantitySelected = unitsByKind.find(quantity => {
+    return quantity.units.some(unit => {
+      return unit.id === idValue;
+    });
+  })
+  return quantitySelected.units.find(unit => {
+      return unit.id === idValue;
+    });
+}
+
+
+// Function to perform conversion manually (without using quantities.js)
+function manualConversion(value, fromUnit, toUnit) {
+  let fromUnitObject = getUnitObjectById(fromUnit);
+  let toUnitObject = getUnitObjectById(toUnit);
+
+  let result = value * fromUnitObject.factor / toUnitObject.factor;
+	
+  return result;
 }
