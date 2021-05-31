f9455eeb7c468b28203c9c14aa3e6bfdd478145e
jAniceto <jose.pedro.aniceto@gmail.com>
2021-05-31 10:55:43 +0100

    Updated site design

--- website/views.py
+++ website/views.py
@@ -34,122 +34,123 @@ def format_scientific_name(name):
 
 
 def home(request):
-	latest_posts = Post.objects.all().order_by('-date_posted')[:3]
-	
-	publications = Publication.objects.all()
-	members = Member.objects.all()
-	awards = Award.objects.all()
-	
-	context = {
-		'page_title': 'Home',
-		'latest_posts': latest_posts,
-		'articles': publications.filter(pub_type='article').count(),
-		'book_chapters': publications.filter(pub_type='book-chapter').count(),
-		'patents': publications.filter(pub_type='patent').count(),
-		'awards': awards.count(),
-		'communications': publications.filter(pub_type='presentation').count(),
-		# 'projects': '9',
-		'MSc_completed': publications.filter(pub_type='thesis', thesis_type='MSc').count(),
-		'MSc_oncourse': members.filter(position='PhD Student').exclude(alumni=True).count(),
-		'fellows_completed': members.filter(position='Research Fellow').exclude(alumni=False).count(),
-		'fellows_oncourse': members.filter(position='Research Fellow').exclude(alumni=True).count(),
-		'PhD_completed': publications.filter(pub_type='thesis', thesis_type='PhD').count(),
-		'PhD_oncourse': members.filter(position='PhD Student').exclude(alumni=True).count(),
-		'postdocs_completed': members.filter(position='PostDoc Researcher').exclude(alumni=False).count(),
-		'postdocs_oncourse': members.filter(position='PostDoc Researcher').exclude(alumni=True).count(),
-	}
-	return render(request, 'website/home.html', context)
+    publications = Publication.objects.all()
+    members = Member.objects.all()
+    awards = Award.objects.all()
+    latest_posts = Post.objects.all().order_by('-date_posted')[:3]
+    latest_publications = publications.order_by('-year', '-date_added')[:5]
+
+    context = {
+        'page_title': 'Home',
+        'latest_posts': latest_posts,
+        'latest_publications': latest_publications,
+        'articles': publications.filter(pub_type='article').count(),
+        'book_chapters': publications.filter(pub_type='book-chapter').count(),
+        'patents': publications.filter(pub_type='patent').count(),
+        'awards': awards.count(),
+        'communications': publications.filter(pub_type='presentation').count() + publications.filter(pub_type='poster').count(),
+        # 'projects': '9',
+        'MSc_completed': publications.filter(pub_type='thesis', thesis_type='MSc').count(),
+        'MSc_oncourse': members.filter(position='PhD Student').exclude(alumni=True).count(),
+        'fellows_completed': members.filter(position='Research Fellow').exclude(alumni=False).count(),
+        'fellows_oncourse': members.filter(position='Research Fellow').exclude(alumni=True).count(),
+        'PhD_completed': publications.filter(pub_type='thesis', thesis_type='PhD').count(),
+        'PhD_oncourse': members.filter(position='PhD Student').exclude(alumni=True).count(),
+        'postdocs_completed': members.filter(position='PostDoc Researcher').exclude(alumni=False).count(),
+        'postdocs_oncourse': members.filter(position='PostDoc Researcher').exclude(alumni=True).count(),
+    }
+    return render(request, 'website/home.html', context)
 
 
 def research(request):
-	context = {
-		'page_title': 'Research',
-		'page_subtitle': 'Core areas of EGICHEM group',
-		'research_fields': ResearchField.objects.all().order_by('order', '-date_added')
-	}
-	return render(request, 'website/research.html', context)
+    context = {
+        'page_title': 'Research',
+        'page_subtitle': 'Core areas of EgiChem group',
+        'research_fields': ResearchField.objects.all().order_by('order', '-date_added')
+    }
+    return render(request, 'website/research.html', context)
 
 
 def lab(request):
-	context = {
-		'page_title': 'Lab',
-		'page_subtitle': 'The EGICHEM laboratory comprises the following research units',
-		'lab_units': LabUnit.objects.all().order_by('order','-date_added')
-	}
-	return render(request, 'website/lab.html', context)
+    context = {
+        'page_title': 'Lab',
+        'page_subtitle': 'Research units and equipment',
+        'lab_units': LabUnit.objects.all().order_by('order','-date_added')
+    }
+    return render(request, 'website/lab.html', context)
 
 
 def partners(request):
-	context = {
-		'page_title': 'Collaborators',
-		'page_subtitle': 'People actively taking part in EGICHEM activities',
-		'page_title2': 'Partners',
-		'page_subtitle2': 'Companies and entities with which EGICHEM has or had some collaborations',
-		'partners': Partner.objects.all().order_by('name'),
-		'collaborators': Collaborator.objects.all().order_by('name')
-	}
-	return render(request, 'website/partners.html', context)
-	
+    context = {
+        'page_title': 'Collaborators',
+        'page_subtitle': 'Academic collaborations',
+        'page_title2': 'Partners',
+        'page_subtitle2': 'Companies that have worked with EgiChem',
+        'partners': Partner.objects.all().order_by('name'),
+        'collaborators': Collaborator.objects.all().order_by('name')
+    }
+    return render(request, 'website/partners.html', context)
+
 
 def people(request):
-	members = Member.objects.all().order_by('name')
-	professors = members.filter(highlighted=True)
-	researchers = members.filter(position='Researcher').exclude(highlighted=True).exclude(alumni=True)
-	postdocs = members.filter(position='PostDoc Researcher').exclude(highlighted=True).exclude(alumni=True)
-	phds = members.filter(position='PhD Student').exclude(highlighted=True).exclude(alumni=True)
-	fellows = members.filter(position='Research Fellow').exclude(highlighted=True).exclude(alumni=True)
-	masters = members.filter(position='MSc Student').exclude(highlighted=True).exclude(alumni=True)
-	undergraduates = members.filter(position='Undergraduate Student').exclude(highlighted=True).exclude(alumni=True)
-	alumni_postdocs = members.filter(alumni=True, position='PostDoc Researcher')
-	alumni_phds = members.filter(alumni=True, position='PhD Student')
-	alumni_fellows = members.filter(alumni=True, position='Research Fellow')
-	alumni_masters = members.filter(alumni=True, position='MSc Student')
-	alumni_undergraduates = members.filter(alumni=True, position='Undergraduate Student')
-	
-	context = {
-		'page_title': 'People',
-		'page_subtitle': 'Researchers, PhD Students, Research Fellows, MSc Students and Undergradurate Students',
-		'professors': professors,
+    members = Member.objects.all().order_by('name')
+    professors = members.filter(highlighted=True)
+    researchers = members.filter(position='Researcher').exclude(highlighted=True).exclude(alumni=True)
+    postdocs = members.filter(position='PostDoc Researcher').exclude(highlighted=True).exclude(alumni=True)
+    phds = members.filter(position='PhD Student').exclude(highlighted=True).exclude(alumni=True)
+    fellows = members.filter(position='Research Fellow').exclude(highlighted=True).exclude(alumni=True)
+    masters = members.filter(position='MSc Student').exclude(highlighted=True).exclude(alumni=True)
+    undergraduates = members.filter(position='Undergraduate Student').exclude(highlighted=True).exclude(alumni=True)
+    alumni_postdocs = members.filter(alumni=True, position='PostDoc Researcher')
+    alumni_phds = members.filter(alumni=True, position='PhD Student')
+    alumni_fellows = members.filter(alumni=True, position='Research Fellow')
+    alumni_masters = members.filter(alumni=True, position='MSc Student')
+    alumni_undergraduates = members.filter(alumni=True, position='Undergraduate Student')
+
+    context = {
+        'page_title': 'People',
+        'page_subtitle': 'EgiChem Team',
+        'professors': professors,
         'researchers': researchers,
-		'postdocs': postdocs,
-		'phds': phds,
-		'fellows': fellows,
-		'masters': masters,
-		'undergraduates': undergraduates,
-		'alumni_postdocs': alumni_postdocs,
-		'alumni_phds': alumni_phds,
-		'alumni_fellows': alumni_fellows,
-		'alumni_masters': alumni_masters,
-		'alumni_undergraduates': alumni_undergraduates,
-	}
-	return render(request, 'website/people.html', context)
+        'postdocs': postdocs,
+        'phds': phds,
+        'fellows': fellows,
+        'masters': masters,
+        'undergraduates': undergraduates,
+        'alumni_postdocs': alumni_postdocs,
+        'alumni_phds': alumni_phds,
+        'alumni_fellows': alumni_fellows,
+        'alumni_masters': alumni_masters,
+        'alumni_undergraduates': alumni_undergraduates,
+    }
+    return render(request, 'website/people.html', context)
 
 
 def publications(request):
-	publications = Publication.objects.all().order_by('-year', '-date_added')
-	articles = publications.filter(pub_type='article')
-	book_chapters = publications.filter(pub_type='book-chapter')
-	patents = publications.filter(pub_type='patent')
-	posters = publications.filter(pub_type='poster')
-	presentations = publications.filter(pub_type='presentation')
-	theses = publications.filter(pub_type='thesis')
-	phd_theses = theses.filter(thesis_type='PhD')
-	msc_theses = theses.filter(thesis_type='MSc')
+    publications = Publication.objects.all().order_by('-year', '-date_added')
+    articles = publications.filter(pub_type='article')
+    book_chapters = publications.filter(pub_type='book-chapter')
+    patents = publications.filter(pub_type='patent')
+    posters = publications.filter(pub_type='poster')
+    presentations = publications.filter(pub_type='presentation')
+    theses = publications.filter(pub_type='thesis')
+    phd_theses = theses.filter(thesis_type='PhD')
+    msc_theses = theses.filter(thesis_type='MSc')
 
-	context = {
-		'page_title': 'Publications',
-		'page_subtitle': 'Our publications are divided in the following categories',
-		'articles': articles,
-		'book_chapters': book_chapters,
-		'patents': patents,
-		'posters': posters,
-		'presentations': presentations,
-		'theses': theses,
-		'phd_theses': phd_theses,
-		'msc_theses': msc_theses,
-		'current_year': datetime.datetime.today().year,
-	}
-	return render(request, 'website/publications.html', context)
+    context = {
+        'page_title': 'Publications',
+        'page_subtitle': 'Our publications are divided in the following categories',
+        'articles': articles,
+        'book_chapters': book_chapters,
+        'patents': patents,
+        'posters': posters,
+        'presentations': presentations,
+        'theses': theses,
+        'phd_theses': phd_theses,
+        'msc_theses': msc_theses,
+        'current_year': datetime.datetime.today().year,
+    }
+    return render(request, 'website/publications.html', context)
 
 
 def member_page(request, name_slug):
@@ -194,22 +195,22 @@ def member_page(request, name_slug):
 
 
 def tools(request):
-	tools = Tool.objects.all().order_by('-date_added')
-	
-	context = {
-		'page_title': 'Tools',
-		'page_subtitle': 'Programs developed by the EgiChem group',
-		'tools': tools,
-	}	
-	return render(request, 'website/tools.html', context)
+    tools = Tool.objects.all().order_by('-date_added')
+
+    context = {
+        'page_title': 'Tools',
+        'page_subtitle': 'Programs developed by the EgiChem group',
+        'tools': tools,
+    }    
+    return render(request, 'website/tools.html', context)
 
 
 def awards(request):
-	awards = Award.objects.all().order_by('-date_added')
+    awards = Award.objects.all().order_by('-date_added')
 
-	context = {
-		'page_title': 'Awards',
-		'page_subtitle': 'Research awards granted to members of the EgiChem group',
-		'awards': awards,
-	}	
-	return render(request, 'website/awards.html', context)
+    context = {
+        'page_title': 'Awards',
+        'page_subtitle': '',
+        'awards': awards,
+    }
+    return render(request, 'website/awards.html', context)
