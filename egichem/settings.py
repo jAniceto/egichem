36bf666499653e7ca8c3c77b572086e9da4be82b
José Aniceto <jAniceto@users.noreply.github.com>
2024-12-21 15:37:21 +0000

    Update settings.py

--- egichem/settings.py
+++ egichem/settings.py
@@ -196,9 +196,10 @@ AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 STATICFILES_DIRS = ( os.path.join(BASE_DIR, "static"), )
 STATIC_URL = 'https://' + AWS_S3_CUSTOM_DOMAIN + '/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_ROOT = 'staticfiles'
 
 # MEDIA_URL = STATIC_URL + 'media/'
-# STATIC_ROOT = 'staticfiles'
+
 # ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 # STATICFILES_FINDERS = (
 #     'django.contrib.staticfiles.finders.FileSystemFinder',
